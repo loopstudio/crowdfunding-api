5edc4dbc3d058fc1ce31cbdbd183eedaef96359c
Renato Cargnelutti <renato.cargnelutti8@gmail.com>
2022-11-10 18:47:47 -0300

    CROW-13 Add CongfigService to ethers service unit tests

--- src/features/ethers/services/ethers.service.spec.ts
+++ src/features/ethers/services/ethers.service.spec.ts
@@ -1,3 +1,4 @@
+import { ConfigService } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
 
 import { EthersService } from './ethers.service';
@@ -7,7 +8,7 @@ describe('EthersService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [EthersService],
+      providers: [EthersService, ConfigService],
     }).compile();
 
     service = module.get<EthersService>(EthersService);
