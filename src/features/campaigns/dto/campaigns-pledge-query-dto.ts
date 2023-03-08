1f67b407747896837fd0ae4b851855fa3aec3d8b
lucasgalarce <galarcelucas@gmail.com>
2023-03-08 09:11:27 -0300

    update dtos

--- src/features/campaigns/dto/campaigns-pledge-query-dto.ts
+++ src/features/campaigns/dto/campaigns-pledge-query-dto.ts
@@ -1,7 +1,20 @@
-import { IsString, IsOptional } from 'class-validator';
-import { PaginationDto } from 'src/common/dto/Pagination.dto';
+import { Type } from 'class-transformer';
+import { IsString, IsOptional, IsNumber, Min } from 'class-validator';
+import { DEFAULT_PAGE, DEFAULT_PAGE_SIZE } from 'src/common/constants';
 
-export class CampaignPledgeQueryDto extends PaginationDto {
+export class CampaignPledgeQueryDto {
+  @IsNumber()
+  @IsOptional()
+  @Min(0)
+  @Type(() => Number)
+  page?: number = DEFAULT_PAGE;
+
+  @IsNumber()
+  @IsOptional()
+  @Min(1)
+  @Type(() => Number)
+  size?: number = DEFAULT_PAGE_SIZE;
+
   @IsString()
   @IsOptional()
   search? = '';
--- src/features/campaigns/dto/campaigns-query-dto.ts
+++ src/features/campaigns/dto/campaigns-query-dto.ts
@@ -1,8 +1,26 @@
 import { Type } from 'class-transformer';
-import { IsString, IsOptional, IsBoolean } from 'class-validator';
-import { PaginationDto } from 'src/common/dto/Pagination.dto';
+import {
+  IsString,
+  IsOptional,
+  IsBoolean,
+  IsNumber,
+  Min,
+} from 'class-validator';
+import { DEFAULT_PAGE, DEFAULT_PAGE_SIZE } from 'src/common/constants';
 
-export class CampaignQueryDto extends PaginationDto {
+export class CampaignQueryDto {
+  @IsNumber()
+  @IsOptional()
+  @Min(0)
+  @Type(() => Number)
+  page?: number = DEFAULT_PAGE;
+
+  @IsNumber()
+  @IsOptional()
+  @Min(1)
+  @Type(() => Number)
+  size?: number = DEFAULT_PAGE_SIZE;
+
   @IsBoolean()
   @IsOptional()
   @Type(() => Boolean)
