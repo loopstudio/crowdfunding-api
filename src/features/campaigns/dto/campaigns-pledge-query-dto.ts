106bf849dd5ff9a4f8c25ed3a406c40eba697902
lucasgalarce <galarcelucas@gmail.com>
2023-05-08 09:30:56 -0300

    Update campaigns pledge query DTO

--- src/features/campaigns/dto/campaigns-pledge-query-dto.ts
+++ src/features/campaigns/dto/campaigns-pledge-query-dto.ts
@@ -1,19 +1,16 @@
-import { Type } from 'class-transformer';
+import { Transform } from 'class-transformer';
 import { IsString, IsOptional, IsNumber, Min } from 'class-validator';
-import { DEFAULT_PAGE, DEFAULT_PAGE_SIZE } from 'src/common/constants';
 
 export class CampaignPledgeQueryDto {
   @IsNumber()
-  @IsOptional()
   @Min(0)
-  @Type(() => Number)
-  page?: number = DEFAULT_PAGE;
+  @Transform(({ value }) => Number(value))
+  readonly page: number;
 
   @IsNumber()
-  @IsOptional()
   @Min(1)
-  @Type(() => Number)
-  size?: number = DEFAULT_PAGE_SIZE;
+  @Transform(({ value }) => Number(value))
+  readonly size: number;
 
   @IsString()
   @IsOptional()
